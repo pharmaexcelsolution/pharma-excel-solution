d0688b0881dde7eb9c7983c2848cc36c96b78031
pharmaexcelsolution <pharmaexcelsolutions@gmail.com>
2026-07-14 00:58:58 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Pharma Excel Solution');
+function openPreview(image){
+
+document.getElementById("lightbox").style.display="flex";
+
+document.getElementById("previewImage").src=image;
+
+}
+
+function closePreview(){
+
+document.getElementById("lightbox").style.display="none";
+
+}
